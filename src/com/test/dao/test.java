b1a85109a2ccf8625bb2034558f6dedb3e4c0bac
李佳玲 <hanhanbiubiu@136.com>
2017-06-10 10:03:01 +0800

    注释：

--- src/com/test/dao/test.java
+++ src/com/test/dao/test.java
@@ -4,11 +4,13 @@ package com.test.dao;
  * Created by 李佳玲 on 2017/6/10.
  */
 public class test {
+    //首先在设置里面配置两个地方version:git.ext和github 的用户名和密码
+    //vcs选择extral...选择git配置完毕
     //删除：git2在github下setting输入工程名字即可
     //下载:在vcs下checkout下github可以下载相应uri的工程下载下来
     //上传:在vcs下import into verison下有个share。点击可以将git2上传至指定位置
-    //
+    //提交:右击可以看到git选项，commit按步骤操作即可
     public static void main(String[] args) {
-        
+
     }
 }
