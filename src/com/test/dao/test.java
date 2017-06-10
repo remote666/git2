066150936168da957f4a72900bbf3032a0a3faa3
李佳玲 <hanhanbiubiu@136.com>
2017-06-10 09:59:18 +0800

    main方法

--- src/com/test/dao/test.java
+++ src/com/test/dao/test.java
@@ -4,4 +4,11 @@ package com.test.dao;
  * Created by 李佳玲 on 2017/6/10.
  */
 public class test {
+    //删除：git2在github下setting输入工程名字即可
+    //下载:在vcs下checkout下github可以下载相应uri的工程下载下来
+    //上传:在vcs下import into verison下有个share。点击可以将git2上传至指定位置
+    //
+    public static void main(String[] args) {
+        
+    }
 }
